3778d47a69dc0263aef7fd52c02f1f1768877864
eliasAlvs <137322132+elijahAlvs@users.noreply.github.com>
2023-08-20 17:07:05 -0300

    Condições iniciada
    Iniciei os estudos de condições em JavaScript

---
+++ CONDICOES/if else.js
@@ -0,0 +1,6 @@
+var velocidade = 60.5
+if(velocidade >= 60){ //quando não tem o senão (else), é chamada de condição simples
+    console.log("Velocidade máxima permitida foi ultrapassada. Você está multado")
+}
+
+console.log('Dirija sempre com cinto de segurança')
