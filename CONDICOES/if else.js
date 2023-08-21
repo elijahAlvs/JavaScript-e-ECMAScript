8e15957aea4798ef98941126772f8154aa87456c
eliasAlvs <137322132+elijahAlvs@users.noreply.github.com>
2023-08-21 12:36:10 -0300

    terminando else if
    quase terminando else if

--- CONDICOES/if else.js
+++ CONDICOES/if else.js
@@ -1,6 +1,34 @@
-var velocidade = 60.5
-if(velocidade >= 60){ //quando não tem o senão (else), é chamada de condição simples
-    console.log("Velocidade máxima permitida foi ultrapassada. Você está multado")
+var idade = 16
+
+console.log(`Você tem ${idade} anos`)
+if(idade < 16){
+    console.log("Você não pode votar")
+} else if(idade < 18 && idade >= 16){
+    console.log("Seu voto é opcional")
+}else{
+    console.log("Seu voto é obrigatório")
 }
 
-console.log('Dirija sempre com cinto de segurança')
+/*
+Criei uma variável que recebe a data (time),
+depois eu só passo o dado específico 
+para variáveis diferentes
+*/ 
+
+var time = new Date() 
+var mes = time.getMonth
+console.log(mes)
+
+var dia = time.getDay
+console.log(dia)
+
+var hora = time.getHours
+console.log(hora)
+
+var min = time.getMinutes
+console.log(min)
+
+var sec = time.getSeconds
+console.log(sec)
+var milliseconds = time.getMilliseconds
+console.log(milliseconds)
