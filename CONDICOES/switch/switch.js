78d9aad46bb50718ac708d48020ba9c719554856
eliasAlvs <137322132+elijahAlvs@users.noreply.github.com>
2023-08-21 20:49:00 -0300

    Estrutura switch finalizado
    Finalizei o estudo de switch

--- CONDICOES/switch/switch.js
+++ CONDICOES/switch/switch.js
@@ -11,3 +11,32 @@ switch(variavel){
         break
 }
 */
+
+var time = new Date()
+var diaSemana = time.getDay()
+
+switch(diaSemana){
+    case 0:
+        console.log("Domingo")
+        break
+    case 1:
+        console.log("Segunda")
+        break
+    case 2:
+        console.log("Terça")
+        break
+    case 3:
+        console.log("Quarta")
+        break
+    case 4:
+        console.log("Quinta")
+        break
+    case 5:
+        console.log("Sexta")
+        break
+    case 6:
+        console.log("Sábado")
+    break
+    default:
+        console.log('Dia inválido')
+}
