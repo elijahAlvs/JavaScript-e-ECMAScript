de42f503136dc7127080c74407cbf4544c760e7c
eliasAlvs <137322132+elijahAlvs@users.noreply.github.com>
2023-08-22 18:32:03 -0300

    Estrutura intermediária do js pronta
    Falta só finilizar os estilos da página através do JavaScript

--- CONDICOES/exercicios-condicoes/assets/js/script.js
+++ CONDICOES/exercicios-condicoes/assets/js/script.js
@@ -2,19 +2,33 @@ var time = new Date()
 var horas = time.getHours()
 
 var textoHorario = document.getElementById("txtHora") 
+var imagem = document.getElementById('img')
+var mainHTML = document.querySelector('main')
 
 if(horas >= 6 && horas <= 12){
     console.log('Manhã')
     textoHorario.innerHTML = `Agora são ${horas} horas`
-    
+    //Estilos
+    document.body.style.background = '#F82A2AD6'
+    mainHTML.style.background = '#FFA74D'
+    imagem.style.background = '#F9C784'
+
 }else if(horas >= 13 && horas <= 18){
     console.log('Tarde')
     textoHorario.innerHTML = `Agora são ${horas} horas`
-}else if((horas >= 19 && horas <=23) || (horas >= 0 && horas <= 5)){
+    //Estilos
+    document.body.style.background = '#F82A2AD6'
+    mainHTML.style.background = '#FFA74D'
+    imagem.style.background = '#F9C784'
 
+}else if((horas >= 19 && horas <=23) || (horas >= 0 && horas <= 5)){
     console.log('Noite')
-    document.body.style.background = '#465775'
     textoHorario.innerHTML = `Agora são ${horas} horas`
+    //Estilos
+    document.body.style.background = '#465775'
+    mainHTML.style.background = '#FFA74D'
+    imagem.style.background = '#F9C784'
+    
 }else{
     console.log('Horário inválido')
     textoHorario.innerHTML = `Horário inválido`
