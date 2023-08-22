12c3b3221aa865af2d337fe0e82aec056364704c
eliasAlvs <137322132+elijahAlvs@users.noreply.github.com>
2023-08-21 21:52:39 -0300

    Está pronto o básico
    Consegui deixar o tempo atualizado

--- CONDICOES/exercicios-condicoes/assets/js/script.js
+++ CONDICOES/exercicios-condicoes/assets/js/script.js
@@ -1,13 +1,18 @@
 var time = new Date()
 var horas = time.getHours()
 
+var textoHorario = document.getElementById("txtHora") 
+
 if(horas >= 6 && horas <= 12){
     console.log('Manhã')
     
 }else if(horas >= 13 && horas <= 18){
     console.log('Tarde')
 }else if((horas >= 19 && horas <=23) || (horas >= 0 && horas <= 5)){
+
     console.log('Noite')
+    document.body.style.background = '#465775'
+    textoHorario.innerHTML = `Agora são ${horas} horas`
 }else{
     console.log('Horário inválido')
 }
