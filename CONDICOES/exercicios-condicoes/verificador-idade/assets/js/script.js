c1322dc7931307f46b33307f1c4bbe6b82bad312
elijahAlvs <137322132+elijahAlvs@users.noreply.github.com>
2023-08-23 15:18:20 -0300

    Básico do JS pronto
    Fiz o básico de condições de JS, mais tarde eu continuo.

--- CONDICOES/exercicios-condicoes/verificador-idade/assets/js/script.js
+++ CONDICOES/exercicios-condicoes/verificador-idade/assets/js/script.js
@@ -1,3 +1,16 @@
 function verificarIdade(){
-    
+    var inputDate = parseInt(document.querySelector('input#iDate').value)
+    var time = new Date()
+    var ano = time.getFullYear()
+    if(ano - inputDate >= 60){
+        console.log('Idoso')
+    }else if((ano - inputDate <= 59) && (ano - inputDate >= 18)){
+        console.log('Adulto')
+    }else if((ano - inputDate >= 12) && (ano - inputDate <= 17)){
+        console.log('Jovem')
+    }else if((ano - inputDate >= 1) && (ano - inputDate <= 11)){
+        console.log('Criança')
+    }else if(inputDate - ano > 0){
+        alert('Digite um ano válido')
+    }
 }
